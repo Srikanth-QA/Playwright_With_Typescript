dc9bea2fe60b1b800d785e2cdb53c44b7d5acb33
Srikanth-QA <srikanthss716@gmail.com>
2026-08-04 23:04:39 +0530

    xpath learning

--- tests/xpathLocators.spec.ts
+++ tests/xpathLocators.spec.ts
@@ -19,4 +19,61 @@ const relativelogo: Locator =
 
 await expect(relativelogo).toBeVisible();
 
+//3. contains()
+
+const products: Locator =
+    page.locator("//h2/a[contains(@href,'computer')]");
+
+const productsCount: number = await products.count();
+console.log("No of computer related products:" ,productsCount);
+expect(productsCount).toBeGreaterThan(0);
+//console.log(await products.testContent());  //Error strict mode violation
+console.log("First computer related product:", await products.first().textContent());
+console.log("Last computer related product:", await products.last().textContent());
+console.log("Nth computer related product:", await products.nth(2).textContent());
+let productTitles: string[] = await products.allTextContents(); // getting all the matched products in to an array
+console.log("All computer related products title:", productTitles)
+for (let pt of productTitles)
+{
+    console.log(pt);
+}
+
+//4. start-with()
+const buildingProducts: Locator =
+    page.locator("//h2/a[starts-with(@href,'/build')]"); // returns multiple elements
+
+const count: number = await buildingProducts.count();
+expect(count).toBeGreaterThan(0);
+
+//5. text() ..
+const reglink: Locator =
+    page.locator("//a[text()='Register']");
+
+await expect(reglink).toBeVisible();
+
+//6. last()
+
+const lastitem: Locator =
+    page.locator("//div[@class='column follow-us']//li[last()]");
+
+await expect(lastitem).toBeVisible();
+
+console.log(
+    "Text content of last element: ",
+    await lastitem.textContent()
+);
+
+
+//7. position()
+
+const positionitem: Locator =
+    page.locator("//div[@class='column follow-us']//li[position(3)]");
+
+await expect(positionitem).toBeVisible();
+
+console.log(
+    "Text content of positional element: ",
+    await positionitem.textContent()
+);
+
 });
